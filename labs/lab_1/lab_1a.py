e6d5355c9a6354c6af340c268b0641c5d90badae
cleecleecleec <clee022121@gmail.com>
2026-03-22 18:07:31 -0400

    Added intro to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Christian Lee" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi guys! My name is Christian Lee, and I am a junior at Stuyvesant High School in Manhattan New York. My favorite subject is definitely physics, and I like playing badminton, video games, and traveling. My favorite ice cream flavor is mint chocolate chip, and I don't understand the controversy behind it.")
 
 if __name__ == "__main__":
     main()
